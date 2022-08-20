ab860a78d4cd58b4e3e119ed8b62a918042d0090
anshul bansal <akbt.tech17@gmail.com>
2022-08-20 12:48:28 +0530

    Created relationship between two classes

--- CodeFirstDemo/Models/Customer.cs
+++ CodeFirstDemo/Models/Customer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodeFirstDemo.Models
 {
@@ -7,11 +9,14 @@ namespace CodeFirstDemo.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
+        public virtual ICollection<Orders> Orders { get; set; }
     }
     public class Orders {
+        [Key]
         public int OrderId { get; set; }
         public DateTime OrderDate { get; set; }
         public string ShippingName { get; set; }
+        public virtual Customer Customer { get; set; }
 
     }
 }
