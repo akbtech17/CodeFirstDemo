139d7676285fd3f75a40d19b5e4a82b2018aca6f
anshul bansal <akbt.tech17@gmail.com>
2022-08-20 12:37:30 +0530

    Created CutomerOder Class

---
+++ CodeFirstDemo/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeFirstDemo.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+    public class Orders {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShippingName { get; set; }
+
+    }
+}
