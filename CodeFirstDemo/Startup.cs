edc4ef6bb4f67372d1fca6fc72071ed232cbfdb9
anshul bansal <akbt.tech17@gmail.com>
2022-08-20 11:53:22 +0530

    Added Mvc service and db context

--- CodeFirstDemo/Startup.cs
+++ CodeFirstDemo/Startup.cs
@@ -1,6 +1,8 @@
+using CodeFirstDemo.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace CodeFirstDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<ShoppingDbContext>(item => item.UseSqlServer(configuration.GetConnectionString("mycon")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
